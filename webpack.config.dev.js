1b0b4e616ea31b1c9c397f4be5bc1887dba3f28a
lucasdmadrigal <lucasmadrigalb@gmail.com>
2021-06-27 21:58:43 -0300

    creado archivo de config para netlify

---
+++ webpack.config.dev.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const DotEnv = require('dotenv-webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+    },
+
+    mode: 'development',
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './index.html',
+            filename: './index.html'
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].[contenthash].css'
+        }),
+        new DotEnv()
+    ],
+}
